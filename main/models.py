a570bd2e6eaaf51c543b1650a2d4a5fcb5360f71
ramawym <ramadhanmaulana446@gmail.com>
2025-09-09 14:52:21 +0700

    add views

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.model):
+class Product(models.Model):
     name = models.CharField()
     price = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'name' : 'Walyul\'ahdi Maulana Ramadhan',
+        'npm' : '2406426012',
+        'class' : 'PBP - F'
+    }
+    
+    return render(request, "main.html", context)
