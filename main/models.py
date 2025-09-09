e364b1df2d553915482dc6823566b675b0a3bd1e
ramawym <ramadhanmaulana446@gmail.com>
2025-09-09 14:45:39 +0700

    add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.model):
+    name = models.CharField()
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField()
+    is_featured = models.BooleanField()
+    rating = models.FloatField()
+    
+    
